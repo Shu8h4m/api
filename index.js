19366e7882824a58466c3857abb0c619cde037cb
Shubham Bhoyar <its.me.shubham6866@gmail.com>
2024-07-19 17:37:31 +0530

    really final

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-    origin: 'https://669a4f4e9413771894f1e40a--inquisitive-tapioca-66bcb6.netlify.app',
+    origin: 'https://669a53c59171a62b6037a6f5--resplendent-kitsune-5efe7a.netlify.app/',
     credentials: true, // This allows the browser to include credentials in the request
   };
   
